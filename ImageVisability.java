c4eceac4b7027f0e4ace286bfc3d4eee88a521f1
10800441 <martyn_heijstek@hotmail.com>
2016-11-04 20:27:45 +0100

    Version 01, Complete and functional Extra button added to quikly set all checkboxes true/false

---
+++ ImageVisability.java
@@ -0,0 +1,17 @@
+package com.example.marty_000.myapp1;
+
+import android.widget.ImageView;
+
+public class ImageVisability
+{
+    private String key;
+    private ImageView image;
+
+    public ImageVisability(String key, ImageView image)
+    {
+        this.key   = key;
+        this.image = image;
+    }
+    public String key()   { return key; }
+    public ImageView image() { return image; }
+}
